dc75825f68ae6f3bd627b6bd93aa05f502cae3a3
kawsikan <kawsinote@gmail.com>
2020-10-15 18:56:50 +0530

    Form validation done for confirmcatering

--- resources/views/catering_orders/confirmcatering.blade.php
+++ resources/views/catering_orders/confirmcatering.blade.php
@@ -120,23 +120,23 @@
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Address</label>
+                            <label class="col-sm-4 col-form-label" for="cus_Address">Address</label>
                             <input type="text" id="cus_Address" placeholder="Your address" name="cus_Address"
                                 class="form-control col-sm-8" required>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Email</label>
-                            <input type="text" id="email" placeholder="Contact email" name="email"
-                                class="form-control col-sm-8" required>
+                            <label class="col-sm-4 col-form-label" for="email">Email</label>
+                            <input type="email" id="email" placeholder="Contact email" name="email"
+                                class="form-control col-sm-8" pattern=".+@.+.com" required>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Telephone</label>
-                            <input type="text" id="telephone_number" placeholder="Contact number"
-                                name="telephone_number" class="form-control col-sm-8" required>
+                            <label class="col-sm-4 col-form-label" for="telephone_number">Telephone</label>
+                            <input type="tel" id="telephone_number" placeholder="10 digit contact number"
+                                name="telephone_number" class="form-control col-sm-8" pattern="[0-9]{10}" required>
                         </div>
                         <br>
 
@@ -148,41 +148,41 @@
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Event Location</label>
+                            <label class="col-sm-4 col-form-label" for="event_location">Event Location</label>
                             <input type="text" id="event_location" placeholder="Address of venue" name="event_location"
                                 class="form-control col-sm-8" required>
                         </div>
                         <br>
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Package code</label>
+                            <label class="col-sm-4 col-form-label" for="package_id">Package code</label>
                             <input type="text" id="package_id" name="package_id" value="{{$cpackages->id}}"
                                 class="form-control col-sm-8" readonly>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Selected package</label>
+                            <label class="col-sm-4 col-form-label" for="catering_name">Selected package</label>
                             <input type="text" id="catering_name" name="catering_name" value="{{$cpackages->p_name}}"
                                 class="form-control col-sm-8" readonly>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Each person</label>
+                            <label class="col-sm-4 col-form-label" for="item_price">Each person</label>
                             <input type="text" id="item_price" name="item_price" value="{{$cpackages->price}}"
                                 class="form-control col-sm-8" readonly>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Guests </label>
+                            <label class="col-sm-4 col-form-label" for="event_guests">Guests </label>
                             <input type="number" id="event_guests" placeholder="Expected number of guests"
                                 onchange="getPrice()" name="event_guests" class="form-control col-sm-8" required>
                         </div>
                         <br>
 
                         <div class="form-row">
-                            <label class="col-sm-4 col-form-label" for="name">Bill Amount</label>
+                            <label class="col-sm-4 col-form-label" for="amount">Bill Amount</label>
                             <input readonly id="amount" placeholder="Total amount" name="amount"
                                 class="form-control col-sm-8">
                         </div>
--- resources/views/catering_orders/viewcorder.blade.php
+++ resources/views/catering_orders/viewcorder.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <title>Catering package</title>
+    <title>Catering orders</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
